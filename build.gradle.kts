5d69ca929704c98441f5a6294b39ad8bd43c0642
XYZboom <xyzboom@qq.com>
2024-05-10 13:03:08 +0800

    <feature> config for jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,60 +4,57 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.0"
-    id("maven-publish")
     id("com.github.johnrengelman.shadow") version "7.0.0"
     application
+    `maven-publish`
 }
 
-group = "com.github.xyzboom"
-version = "1.0-SNAPSHOT"
+group = "com.github.XYZboom"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenLocal()
-    maven {
-        url = uri("http://47.115.213.131:8080/repository/alex-release/")
-        isAllowInsecureProtocol = true
-    }
-    maven {
-        url = uri("http://47.115.213.131:8080/repository/alex-snapshots/")
-        isAllowInsecureProtocol = true
-    }
     mavenCentral()
+    maven("https://jitpack.io")
 }
-
 publishing {
     repositories {
-        maven {
-            url = uri("http://47.115.213.131:8080/repository/alex-snapshots/")
-            credentials {
-                username = properties["maven-username"].toString()
-                password = properties["maven-password"].toString()
-            }
-            isAllowInsecureProtocol = true
-        }
+        mavenLocal()
     }
     publications {
-        create<MavenPublication>("maven") {
-            groupId = "cn.emergentdesign.se"
+        create<MavenPublication>("depend-core") {
+            groupId = "com.github.XYZboom"
             artifactId = "depends-kotlin"
             version = "1.0.0-SNAPSHOT"
 
             from(components["java"])
         }
+        create<MavenPublication>("depend-core-source") {
+            groupId = "com.github.XYZboom"
+            artifactId = "depends-kotlin"
+            version = "1.0.0-SNAPSHOT"
+
+            // 配置要上传的源码
+            artifact(tasks.register<Jar>("sourcesJar") {
+                from(sourceSets.main.get().allSource)
+                archiveClassifier.set("sources")
+            }) {
+                classifier = "sources"
+            }
+        }
     }
 }
-
 dependencies {
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
     implementation("org.slf4j:slf4j-log4j12:2.0.10")
 //    implementation("ch.qos.logback:logback-classic:1.4.7")
-    implementation("cn.emergentdesign.se:depends-core:0.9.8-SNAPSHOT")
-    implementation("cn.emergentdesign.se:depends-java:0.9.8-SNAPSHOT")
+    implementation("com.github.XYZboom:depends-core:1.0.0-alpha6")
+    implementation("com.github.XYZboom:depends-java:1.0.0-alpha3")
     implementation("org.antlr:antlr4:4.13.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("cn.emergentdesign.se:utils:0.1.1")
+    testImplementation("com.github.multilang-depends:utils:04855aebf3")
 }
 
 tasks.test {
